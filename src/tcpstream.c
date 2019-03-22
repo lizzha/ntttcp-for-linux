1b8f2a35c5d7e7f0661d65d626b98c8f07d49dcc
Liz Zhang <lizzha@microsoft.com>
2019-03-22 15:00:54 +0800

    Test

--- src/tcpstream.c
+++ src/tcpstream.c
@@ -40,7 +40,8 @@ int n_write(int fd, const char *buffer, size_t total)
 	register size_t left = total;
 
 	while (left > 0) {
-//		rtn = write(fd, buffer, left);
+		usleep(1000);
+		rtn = write(fd, buffer, left);
 		rtn = 0;
 		if (rtn < 0) {
 			if (errno == EINTR || errno == EAGAIN) {
